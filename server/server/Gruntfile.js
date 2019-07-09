7a27d4e0db7fad6d5249efd564085b4ee41f964e
Dustin Hickman <43394966+S3kwel@users.noreply.github.com>
2019-07-09 14:47:58 -0400

    Sass, templating progress
    Implemented Sass, grunt to handle compiling.  Spell template functionally complete.

---
+++ server/server/Gruntfile.js
@@ -0,0 +1,20 @@
+﻿/// <binding BeforeBuild='sass' />
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: {
+                    'public/stylesheets/main.css': 'sass/main.scss' // 'destination':'source'
+                },
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', ['sass']);
+}
