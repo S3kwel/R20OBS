39bbfb86e201d86a8c56ff9c036f3d812b960e54
Dustin Hickman <43394966+S3kwel@users.noreply.github.com>
2019-11-19 07:20:05 -0500

    Various

--- src/scripts/aesthetic.js
+++ src/scripts/aesthetic.js
@@ -1,4 +1,26 @@
 "use strict"
 $(function () {
-    $(`<div id = 'borderBox'>TEST</div>`).insertAfter(`#finalCanvas`); 
+    //Hide the chat
+	$('#sidebarcontrol').click(); 
+	
+	
+	
+	//Set Zoom Level...
+	var slider = $('#zoomslider').find('a').eq(0);
+		console.log(slider); 
+		
+		//Only works in popup mode.  
+		window.resizeTo(1280, 720);
+	
+		setInterval(function(){
+		slider.slider('value',50);
+		
+		
+		},500);
+		$('#zoomslider').find('a').click();
+
+                                     
+		//slider.remove(); 
+	
+	$('.ui-slider-handle.ui-state-default.ui-corner-all').click(); 
 });
