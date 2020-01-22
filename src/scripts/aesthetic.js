5a7db5caf92364bb0d09077f76a347830ef69102
Dustin Hickman <43394966+S3kwel@users.noreply.github.com>
2020-01-21 21:21:19 -0500

    BG Color Overlay
    Color overlay now present over Streaming Mode video.
    
    Overlay color dynamically changes when a new page loads.

--- src/scripts/aesthetic.js
+++ src/scripts/aesthetic.js
@@ -1,6 +1,7 @@
 let type = "setmode";
 let title = "Streaming Mode"; 
 let url = $('script.R20OBS_SCRIPT').eq(0).data('extension'); 
+var inStreamingMode = false;
 
 
 
@@ -16,13 +17,28 @@ var streamingMode = `
         <source src="${url}images/magic.mp4" type="video/mp4">
     </video>`; 
 
+    var overlayTemplate = `
+    <div id="R20OBSOVERLAY">
+       
+    </div>`; 
+
     $('#page-toolbar').append(streamingMode);
     $('.tabmenu').find('.setmode').addClass('tabmenu'); 
 
-   
+
+
 
     function enterStreamingMode() {
-        
+        if (!inStreamingMode) {
+            inStreamingMode = true;
+        }
+        else {
+            inStreamingMode = false; 
+        }
+
+        //Grab the BG color from the engine.
+        var overlayColor = d20.engine.backgroundColor;
+       
 
         var sideBarState = ($("#rightsidebar").css('display') == 'none') ? false : true;
         let sheet = $('#streamsheet');
@@ -37,13 +53,18 @@ var streamingMode = `
         //Check for the streaming stylesheet.  Add it, if needed.  
         if (sheet.length == 0) {
             $('head').append(`<link ID = "streamsheet" rel='stylesheet' type="text/css" href="${url}styles/stream.css">`);
-            $(bgTemplate).insertAfter("#editor-wrapper")
+            $(bgTemplate).insertAfter("#editor-wrapper");
+            $(overlayTemplate).insertAfter("#R20OBSBG");
+            $("#R20OBSOVERLAY").css('background-color',overlayColor)
             return;
         }
 
         //Otherwise, remove it.
         $('#streamsheet').remove();
         $("#R20OBSBG").remove();
+        $("#R20OBSOVERLAY").remove(); 
+
+        
         sidebarRight = $("#sidebarcontrol").css('right');
 
         //If false, click to show. 
@@ -52,6 +73,13 @@ var streamingMode = `
         }
     }
 
+
+    $(document).on('d20:pagechanged', function () {
+
+        var overlayColor = d20.engine.backgroundColor;
+        
+        $("#R20OBSOVERLAY").css('background-color',overlayColor); 
+    }); 
 
     //Handler for "Extension Visuals" button
     $('#sidebarcontrol').click(function (event) {
@@ -64,3 +92,4 @@ var streamingMode = `
     $('body').on('click', '.setmode',enterStreamingMode); 
 
 
+   
